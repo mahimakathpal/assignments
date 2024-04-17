94ac38c14030de86f7bf56f9b229766e21afe032
mahimakathpal <145335328+mahimakathpal@users.noreply.github.com>
2024-04-17 11:40:25 +0530

    Update findLargestElement.js

--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -6,6 +6,14 @@
 */
 
 function findLargestElement(numbers) {
+     let max = numbers[0];
+    for (let index = 1; index < numbers.length; index++) {
+        if (numbers[index] > max) {
+            max = numbers[index];
+        }
+    }
+    return max;
+
     
 }
 
